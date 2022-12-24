08975fe7389805c0ef2cf0129d22b405a74e0f5a
niknal357 <46793548+niknal357@users.noreply.github.com>
2022-12-24 09:41:36 +0200

    Update legacy.py

--- encoders/legacy.py
+++ encoders/legacy.py
@@ -6,12 +6,12 @@ class Legacy:
         self.encodedInstructions = np.empty(0, int)
 
 
-    def addInstruction(self, pos, command, xyzScail = 16):
+    def addInstruction(self, pos, command, xyzScale = 16):
         self.encodedInstructions = np.append(self.encodedInstructions,
             pos[0]+
-            pos[1]*xyzScail+
-            pos[2]*xyzScail**2+
-            command*xyzScail**3
+            pos[1]*xyzScale+
+            pos[2]*xyzScale**2+
+            command*xyzScale**3
         )
     
     def getInstructions(self):
